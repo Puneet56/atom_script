8c8787dacb72eb9c888ec7ee5a2e6b8490a2c737
Puneet Dhiman <puneetdhiman56@gmail.com>
2023-11-18 22:54:52 +0530

    .

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -19,25 +19,6 @@ func isError(obj object.Object) bool {
 	return false
 }
 
-var builtins = map[string]*object.Builtin{
-	"len": {
-		Fn: func(args ...object.Object) object.Object {
-			if len(args) != 1 {
-				return newError("wrong number of arguments. got=%d, want=1",
-					len(args))
-			}
-
-			switch arg := args[0].(type) {
-			case *object.String:
-				return &object.Integer{Value: int64(len(arg.Value))}
-			default:
-				return newError("argument to `len` not supported, got %s",
-					args[0].Type())
-			}
-		},
-	},
-}
-
 // Eval function
 func Eval(node ast.Node, env *object.Environment) object.Object {
 	switch node := node.(type) {
